4a20d96511e2ea34bf88eeb8b2ee4e675284eb87
Kim, Hyungjun <gudwnd1243@gmail.com>
2021-10-26 09:33:22 +0900

    201633014-3

--- src/git_exam2/Hello.java
+++ src/git_exam2/Hello.java
@@ -2,6 +2,7 @@ package git_exam2;
 
 public class Hello {
 	public static void main(String[] args) {
+		System.our.println("IT융합자율학부");
 		System.out.println(" 201633014 ");
 		System.out.println(" 김형준 ");
 	}
